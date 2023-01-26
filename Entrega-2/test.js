4c01836b4493110e95c1b07cec29e22abe985ca6
Lucas Camarotta <lucas_camarotta@hotmail.com>
2023-01-25 21:47:36 -0300

    Segunda entrega

---
+++ Entrega-2/test.js
@@ -0,0 +1,38 @@
+import ProductManager from "./productManager.js";
+const myProductsManager = new ProductManager();
+
+//console.log('should get empty array if file is not found')
+//console.log(await myProductsManager.getProducts(), '\n')
+
+// console.log('add product')
+// await myProductsManager.addProduct('Banana', 'Yellow', 20, 'thumbnail', 'afg6a541', 50)
+
+// console.log('add product')
+// await myProductsManager.addProduct('Orange', 'Orange', 10, 'thumbnail', '6546a2d1', 10)
+
+// console.log('add product')
+// await myProductsManager.addProduct('Anana', 'White', 90, 'thumbnail', 'hfg6d5as', 30)
+
+//console.log('add product with same code')
+//await myProductsManager.addProduct('Apple', 'Green', 10, 'thumbnail', 'hfg6d5as', 70)
+
+//console.log('add product missing info')
+//await myProductsManager.addProduct('Anana', 'White', 90)
+
+//console.log('get prod by ID 22')
+//console.log(await myProductsManager.getProductsById(22))
+
+// console.log('get prod by ID 2')
+// console.log(await myProductsManager.getProductsById(2),'\n')
+
+//console.log('Update product ID 3')
+//console.log(await myProductsManager.updateProduct(3, 'Anana', 'White', 110, 'thumbnail', 'hfg6d5as', 40))
+
+//console.log('Get all products')
+//console.log(await myProductsManager.getProducts())
+
+//console.log('Delete product ID 1')
+//console.log(await myProductsManager.deleteProduct(1))
+
+//console.log('Get all products')
+//console.log(await myProductsManager.getProducts())
