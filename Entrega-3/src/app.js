24960164132783bd0f95b9b45ae64006b55d7449
Lucas Camarotta <lucas_camarotta@hotmail.com>
2023-01-27 22:28:31 -0300

    Tercera entrega

--- Entrega-3/src/app.js
+++ Entrega-3/src/app.js
@@ -1,11 +1,29 @@
 import express from 'express';
 import ProductManager from "./productManager.js";
-const myProductsManager = new ProductManager();
 
 const app = express();
+const productsManager = new ProductManager();
 
-app.get('/', (req, res) => {
-  res.send('Server')
+app.use(express.urlencoded({extended:true}))
+
+app.get('/products', async(req, res) => {
+  let limit = req.query;
+  limit = Number(limit.limit)
+  const products = await productsManager.getProducts();
+  if(!limit){
+    res.send({products});
+  } else{
+    while(products.length > limit){
+      products.pop();
+    }
+    res.send({products})
+  }
+});
+
+app.get('/products/:pid', async(req, res) => {
+  const productId = Number(req.params.pid);
+  const product = await productsManager.getProductsById(productId);
+  res.send({product})
 });
 
 app.listen(8080, () => console.log('Listening on port 8080'));
