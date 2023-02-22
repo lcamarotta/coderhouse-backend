0c5afb17f1d8ca1810ad4a4f23f88e617b08c932
Lucas Camarotta <lucas_camarotta@hotmail.com>
2023-02-22 04:34:36 -0300

    delete not used files

--- src/utils/customError.js
+++
@@ -1,8 +0,0 @@
-class customError {
-    constructor(httpStatusCode, msg) {
-        this.httpStatusCode = httpStatusCode;
-        this.msg = msg;
-    }
-}
-
-export default customError;
--- src/utils/pathToFile.js
+++
@@ -1,9 +0,0 @@
-import { fileURLToPath } from "url";
-import path, { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename)
-
-const pathToFile = (string) => path.join(__dirname, '..',string)
-
-export default pathToFile;
